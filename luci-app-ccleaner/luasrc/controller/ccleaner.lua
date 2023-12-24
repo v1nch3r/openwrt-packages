c895d8b6ffd2688336e2ee427abae19d2532c3f2
v1nch3r <musarrum@gmail.com>
2023-12-24 19:16:31 +0700

    add

--- luci-app-ccleaner/luasrc/controller/ccleaner.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.ccleaner",package.seeall)
-function index()
-	entry({"admin","status","ccleaner"}, call("ccleaner"), _("CCleaner"), 6)
-end
-function ccleaner()
-	luci.sys.call("/sbin/clearcache.sh >/dev/null 2>&1")
-	luci.http.redirect(luci.dispatcher.build_url("admin/status"))
-end
